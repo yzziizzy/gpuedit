f2cd5ae9f4bcfc3971815c19351dc309e72561c4
izzy <yzziizzy@gmail.com>
2021-03-19 19:39:18 -0600

    basic lua highlighter does not support the weird [===[ strings

--- src/mainControl.c
+++ src/mainControl.c
@@ -557,6 +557,7 @@ GUIMainControl* GUIMainControl_New(GUIManager* gm, GlobalSettings* gs) {
 	Highlighter_LoadModule(&w->hm, "/usr/lib64/gpuedit/highlighters/c.so");
 	Highlighter_LoadModule(&w->hm, "/usr/lib64/gpuedit/highlighters/js.so");
 	Highlighter_LoadModule(&w->hm, "/usr/lib64/gpuedit/highlighters/py.so");
+	Highlighter_LoadModule(&w->hm, "/usr/lib64/gpuedit/highlighters/lua.so");
 	
 	
 	// TODO: resize
